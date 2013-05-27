cfb275b6dbd9ebb064bc951b026ba8afbaee5d7a
Tony Hadfield <thadfield@gmail.com>
2013-05-27 12:11:17 -0700

    Trying to globalize the site name

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,7 +5,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "My Faith Shines"
+  config.site_title = $sitename
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
