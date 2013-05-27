52f978f1ceb5539dc1c29ef79fd24d7b76424259
Tony Hadfield <thadfield@gmail.com>
2013-05-27 13:28:49 -0700

    fixing list of dogs in admin site...

--- app/admin/dogs.rb
+++ app/admin/dogs.rb
@@ -5,12 +5,12 @@ ActiveAdmin.register Dog do
   form :html => { :enctype => "multipart/form-data" } do |f|
      f.inputs "Details" do
       f.input :name
-      f.input :gender, :as => :select, :include_blank => false
+ #     f.input :gender, :as => :select, :include_blank => false
       f.input :home
       f.input :age
-      f.input :motto
-      f.input :more
-      f.input :dog_type, :as => :select, :include_blank => false
+ #     f.input :motto
+ #     f.input :more
+ #     f.input :dog_type, :as => :select, :include_blank => false
       f.input :image, :as => :file
     end
     f.actions
@@ -22,12 +22,12 @@ ActiveAdmin.register Dog do
     column :name
     column :age
     column :home
-    column :motto do |dog|
-       dog.more.nil? ? '' : truncate(dog.motto.html_safe, :length => 50)  
-    end   
-    column :more do |dog|
-       dog.more.nil? ? '' : truncate(dog.more.html_safe, :length => 100)  
-    end
+ #   column :motto do |dog|
+ #      dog.more.nil? ? '' : truncate(dog.motto.html_safe, :length => 50)  
+ #   end   
+ #   column :more do |dog|
+ #     dog.more.nil? ? '' : truncate(dog.more.html_safe, :length => 100)  
+ #  end
  
    default_actions
   end 
