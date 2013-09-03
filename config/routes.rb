3dd0ee5dd767822160e151da4eb80bd7438c05aa
Tony Hadfield <thadfield@gmail.com>
2013-09-02 23:19:46 -0700

    fixing route, not sure

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ GoodDog::Application.routes.draw do
   
   match '/nonprofit/:id' => 'non_profits#show', :as => :nonprofit_view
   match '/nonprofit/:id/:dog_id' => 'non_profits#show', :as => :nonprofit_with_dog_view
+  match '/nonprofit/new' => 'non_profits#new', :as => :nonprofit_new
    
   match '/add_gift_code/:non_profit_id/:gift_code' => 'reservations#validate_gift_code', :as=> :add_gift_code
   match '/validate_gift_codes/:non_profit_id' => 'reservations#validate_gift_code_list', :as=> :add_gift_code_list
