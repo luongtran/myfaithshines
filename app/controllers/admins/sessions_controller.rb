62cdac669f28800d4286b0fa54343b2483f51be8
Tony Hadfield <thadfield@gmail.com>
2013-09-08 22:30:24 -0700

    undoing changes

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -19,7 +19,7 @@ class Admins::SessionsController < Devise::SessionsController
   end
 
   def new
-    self.resource = build_resource(nil)
+    self.resource = build_resource(nil, :unsafe => true)
     clean_up_passwords(resource)
     respond_with(resource, serialize_options(resource))
   end
