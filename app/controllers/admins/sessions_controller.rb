502877a3b98b1f930dd1fca1579c30bd17f3c38f
Tony Hadfield <thadfield@gmail.com>
2013-09-08 22:34:13 -0700

    playing

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -19,9 +19,9 @@ class Admins::SessionsController < Devise::SessionsController
   end
 
   def new
-    self.resource = build_resource(nil, :unsafe => true)
-    clean_up_passwords(resource)
-    respond_with(resource, serialize_options(resource))
+    #self.resource = build_resource(nil, :unsafe => true)
+    #clean_up_passwords(resource)
+    #respond_with(resource, serialize_options(resource))
   end
   
    # POST /resource/sign_in
